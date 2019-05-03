f1643afe5eee33e7fa4176275f32f288148638e5
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2019-05-03 11:33:06 +0300

    wip

--- platform/android/src/main.cpp
+++ platform/android/src/main.cpp
@@ -1,8 +1,15 @@
 #include "jni.hpp"
 #include <jni/jni.hpp>
 
+#include <mbgl/mbgl.hpp>
+
 extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *) {
     assert(vm != nullptr);
+
+    // Deliberately not calling mbgl::Cleanup
+    // because JNI_OnUnload is useless.
+    mbgl::Init();
+
     mbgl::android::registerNatives(vm);
     return JNI_VERSION_1_6;
 }
--- platform/macos/app/main.m
+++ platform/macos/app/main.m
@@ -1,5 +1,13 @@
 #import <Cocoa/Cocoa.h>
 
+#include <mbgl/mbgl.hpp>
+
 int main(int argc, const char * argv[]) {
-    return NSApplicationMain(argc, argv);
+    mbgl::Init();
+
+    int ret = NSApplicationMain(argc, argv);
+
+    mbgl::Cleanup();
+
+    return ret;
 }
